c2959bc715c7aeca3f12d8f82198364a91e2aba1
khanshorif331 <khanshorif331@gmail.com>
2022-07-25 21:17:19 +0600

    config file initialized

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const dotenv = require('dotenv')
 const PORT = process.env.PORT || 5000
+
+dotenv.config()
+
+// middleware
+app.use(cors())
+app.use(express.json())
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`)
