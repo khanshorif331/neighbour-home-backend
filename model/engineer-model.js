eab5dcb931abfd7e50dce42c579b49e385eee8f5
khanshorif331 <khanshorif331@gmail.com>
2022-07-26 02:06:24 +0600

    created schema for the engineers

--- model/engineer-model.js
+++ model/engineer-model.js
@@ -1,5 +1,18 @@
 const mongoose = require('mongoose')
 
 const engineerSchema = new mongoose.Schema({
-	name: String,
+	name: {
+		type: String,
+		required: true,
+		minlength: 3,
+		maxlength: 25,
+	},
+	picture: String,
+	gender: {
+		type: String,
+		enum: ['male', 'female'],
+	},
+	surname: String,
 })
+
+module.exports = engineerSchema
