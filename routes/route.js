7f0557f039e1f6cb0066c1d6697541f6e4f156ed
khanshorif331 <khanshorif331@gmail.com>
2022-07-26 21:33:00 +0600

    created posting all engineers route perfectly

--- routes/route.js
+++ routes/route.js
@@ -1,10 +1,16 @@
 const express = require('express')
-const engineerProfile = require('../controller/engineer-controller.js')
+// const engineerProfile = require('../controller/engineer-controller.js')
+// const insertMultipleEngineers = require('../controller/engineer-controller.js')
+const {
+	engineerProfile,
+	insertMultipleEngineers,
+} = require('../controller/engineer-controller.js')
 
 const router = express.Router()
 
 // const engineer = require('../model/engineer-model')
 
 router.post('/engineer', engineerProfile)
+router.post('/engineer/all', insertMultipleEngineers)
 
 module.exports = router
