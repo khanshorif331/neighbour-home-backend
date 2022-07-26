a9a9fcbd65fcfbd3ae4b3071d7337590d09f1bac
khanshorif331 <khanshorif331@gmail.com>
2022-07-26 21:33:49 +0600

    updated some schema

--- routes/route.js
+++ routes/route.js
@@ -1,14 +1,10 @@
 const express = require('express')
-// const engineerProfile = require('../controller/engineer-controller.js')
-// const insertMultipleEngineers = require('../controller/engineer-controller.js')
 const {
 	engineerProfile,
 	insertMultipleEngineers,
 } = require('../controller/engineer-controller.js')
 
 const router = express.Router()
-
-// const engineer = require('../model/engineer-model')
 
 router.post('/engineer', engineerProfile)
 router.post('/engineer/all', insertMultipleEngineers)
