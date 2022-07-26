3a3625e01caa7a812895ad594a35bd26252b71f8
khanshorif331 <khanshorif331@gmail.com>
2022-07-27 00:27:55 +0600

    created controller for getting all constructors

--- controller/constructor-controller.js
+++ controller/constructor-controller.js
@@ -3,17 +3,6 @@ const Constructor = require('../model/constructor-model.js')
 // posting a single constructor
 const insertConstructor = async (req, res) => {
 	try {
-		// const exist = await User.findone({ username: req.body.username })
-		// console.log(exist)
-		// if (exist) {
-		// 	return res.status(401).json({
-		// 		message: 'Username already exists',
-		// 	})
-		// }
-
-		// const engineers = await Engineer.find({})
-		// res.send(engineers)
-
 		const constructor = req.body
 		const newConstructor = new Constructor(constructor)
 		await newConstructor.save()
@@ -26,6 +15,18 @@ const insertConstructor = async (req, res) => {
 	}
 }
 
-module.exports = insertConstructor
+// getting all constructor data
+const getAllConstructor = async (req, res) => {
+	try {
+		const data = await Constructor.find({})
+		res.status(200).json({
+			result: data,
+		})
+	} catch (err) {
+		res.status(500).json({ message: 'There was a server side error!' })
+	}
+}
+
+module.exports = { insertConstructor, getAllConstructor }
 
 // module.exports = insertConstructor
