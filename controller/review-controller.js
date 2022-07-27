8dea70e6fe8ee16191210ac6542012701fa54c13
khanshorif331 <khanshorif331@gmail.com>
2022-07-27 23:16:50 +0600

    file changed in the review controoller

--- controller/review-controller.js
+++ controller/review-controller.js
@@ -1,19 +1,8 @@
 const Review = require('../model/review-model')
 
-// posting a single engineer
+// posting a single review
 const singleReview = async (req, res) => {
 	try {
-		// const exist = await User.findone({ username: req.body.username })
-		// console.log(exist)
-		// if (exist) {
-		// 	return res.status(401).json({
-		// 		message: 'Username already exists',
-		// 	})
-		// }
-
-		// const engineers = await Engineer.find({})
-		// res.send(engineers)
-
 		const review = req.body
 		const newReview = new Review(review)
 		await newReview.save()
