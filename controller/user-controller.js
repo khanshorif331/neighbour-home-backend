0aa3b8383f0d8261415aee58aaa0763f870744f0
khanshorif331 <khanshorif331@gmail.com>
2022-07-28 22:40:49 +0600

    getall user function added too get all user

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -17,4 +17,14 @@ const postUser = async (req, res) => {
 	}
 }
 
-module.exports = postUser
+// getting all user data
+const getAllUser = async (req, res) => {
+	try {
+		const data = await User.find()
+		res.status(200).json(data)
+	} catch (err) {
+		res.status(500).json({ message: 'There was a server side error!' })
+	}
+}
+
+module.exports = { postUser, getAllUser }
