a02352c0129a6ddd7fcb36bf4a14d2cdc4e5486c
khanshorif331 <khanshorif331@gmail.com>
2022-07-28 19:22:19 +0600

    user moodel created

--- model/user-model.js
+++ model/user-model.js
@@ -1,6 +1,52 @@
 const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema({})
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		minlength: 3,
+		maxlength: 50,
+	},
+	username: {
+		type: String,
+		required: true,
+		minlength: 5,
+		unique: true,
+		maxlength: 50,
+	},
+	address: {
+		type: String,
+		required: true,
+		minlength: 5,
+		maxlength: 255,
+	},
+	zip: {
+		type: Number,
+		required: true,
+	},
+	phone: {
+		type: Number,
+		required: true,
+		minlength: 7,
+		maxlength: 15,
+	},
+	country: {
+		type: String,
+		required: true,
+		minlength: 3,
+	},
+	nid: {
+		type: String,
+		required: true,
+		minlength: 11,
+		maxlength: 20,
+	},
+	photo: String,
+	role: {
+		type: String,
+		enum: ['engineer,worker,buyer,admin'],
+	},
+})
 
 const user = mongoose.model('user', userSchema)
 
