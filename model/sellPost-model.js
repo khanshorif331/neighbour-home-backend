c5411bf4d9589c4e2b7405edd55727a14898f882
khanshorif331 <khanshorif331@gmail.com>
2022-08-11 23:37:32 +0600

    sellpost model created

---
+++ model/sellPost-model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const sellPostSchema = new mongoose.Schema({
+	propertyName: String,
+	propertyType: String,
+	sellerName: String,
+	images: String,
+	propertyPrice: Number,
+	rating: Number,
+	width: String,
+	length: String,
+	location: String,
+	totalarea: String,
+	selingarea: String,
+})
+
+const sellPost = mongoose.model('sellPost', sellPostSchema)
+
+module.exports = sellPost
