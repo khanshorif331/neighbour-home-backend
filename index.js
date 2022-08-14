52cec6bee488e257c4e682a91662f78f2914b9c5
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-08-14 19:58:05 +0600

    payment method succefuly fix error by rakib

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const dotenv = require('dotenv')
-const Stripe = require('stripe')
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+const Stripe = require('stripe');
+const stripe = Stripe('sk_test_51L1nmKGPq7AV2lfoMBfMhIyvnC3qDye5Za6MatNwxCHBnYuiV6xXt9Ad2cGN7f7YheVhaxaSFlmLGJB9SEN2WmcK00fVZldDDq');
 const PORT = process.env.PORT || 5000
 
 const Connection = require('./database/db')
 const router = require('./routes/route')
+
 
 dotenv.config()
 
@@ -21,24 +22,25 @@ const PASSWORD = process.env.DB_PASSWORD
 
 Connection(USERNAME, PASSWORD)
 
-app.post('/create-payment-intent', async (req, res) => {
-	try {
-		const { propertyPrice } = req.body
-		const amount = propertyPrice * 100
-
+// create payment 
+async function run () {
+	app.post("/create-payment-intent", async (req, res) => {
+		const { propertyPrice } = req.body;
+		const amount = propertyPrice * 100;
+	
 		// Create a PaymentIntent with the order amount and currency
 		const paymentIntent = await stripe.paymentIntents.create({
 			amount: amount,
-			currency: 'usd',
-			payment_method_types: ['card'],
-		})
+			currency: "usd",
+			payment_method_types: ['card']
+		});
 		res.send({
 			clientSecret: paymentIntent.client_secret,
-		})
-	} catch (err) {
-		console.log(err)
-	}
-})
+		});
+	});
+}
+run()
+
 
 app.use('/', router)
 
